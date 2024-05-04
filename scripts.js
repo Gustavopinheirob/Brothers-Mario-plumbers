087224169e3469590e8e1666f70d0bd03e09db15
gustavopinheirob <gustavopinheiro1365@gmail.com>
2024-05-03 22:09:31 -0300

    Projeto Mario Bros 1.0

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const form = document.querySelector(".fale-conosco")
+const mascara = document.querySelector(".mascara-form")
+
+function cliqueinobotao(){
+    form.style.left = "50%"
+    form.style.transform= "translateX(-50%)"
+    mascara.style.visibility="visible"
+}
+
+function sairdoform(){
+    form.style.left = "-300px"
+    form.style.transform = "translateX(0)"
+    mascara.style.visibility= "hidden"
+}
+
+function faleconosco(){
+    form.style.left = "50%"
+    form.style.transform= "translateX(-50%)"
+    mascara.style.visibility="visible"
+}
